b7008254a4a2e1c71d776bf79696c4ca706add1d
sugu <sugu@2dfire.com>
2019-03-24 14:37:54 +0800

    新增了加法

--- src/main/java/com/sugu/solution/Add.java
+++ src/main/java/com/sugu/solution/Add.java
@@ -7,7 +7,15 @@ package com.sugu.solution;
  https://www.nowcoder.com/practice/59ac416b4b944300b617d4f7f111b215?tpId=13&tqId=11201&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
  */
 public class Add {
+    // 剑指offer的原解法，主要是就是保留一个和和一个进位
     public int Add(int num1,int num2) {
-        return num1 ^ num2;
+        int sum,carry;
+        do{
+            sum = num1 ^ num2;
+            carry = (num1 & num2) << 1;
+            num1 = sum;
+            num2 = carry;
+        }while (carry != 0);
+        return sum;
     }
 }
