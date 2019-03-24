af09279af38260be51c1482276af88ed32b96023
sugu <sugu@2dfire.com>
2019-03-24 15:39:22 +0800

    新增了题解

---
+++ src/main/java/com/sugu/solution/JumpFloorII.java
@@ -0,0 +1,14 @@
+package com.sugu.solution;
+
+/**
+ * 题目描述
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ * 链接:
+ https://www.nowcoder.com/practice/22243d016f6b47f2a6928b4313c85387?tpId=13&tqId=11162&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class JumpFloorII {
+    public int JumpFloorII(int target) {
+
+        return 0;
+    }
+}
