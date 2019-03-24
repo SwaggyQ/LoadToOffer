03e81537fe1e5dd937c86d33795ae96a6ecbfde7
sugu <sugu@2dfire.com>
2019-03-24 15:38:51 +0800

    新增了题解

--- src/main/java/com/sugu/solution/TreeDepth.java
+++ src/main/java/com/sugu/solution/TreeDepth.java
@@ -4,6 +4,8 @@ package com.sugu.solution;
  * Created by sugu on 2019/3/24.
  */
 
+import com.sugu.solution.base.TreeNode;
+
 /**
  * 题目描述
  * 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
--- src/main/java/com/sugu/solution/base/TreeNode.java
+++ src/main/java/com/sugu/solution/base/TreeNode.java
@@ -1,12 +1,12 @@
-package com.sugu.solution;
+package com.sugu.solution.base;
 
 /**
  * Created by sugu on 2019/3/24.
  */
 public class TreeNode {
     int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
+    public TreeNode left = null;
+    public TreeNode right = null;
 
     public TreeNode(int val) {
         this.val = val;
