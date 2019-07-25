07cfd69e6dcb343ef2d0d9bf664c0aa0cd603acb
sugu <sugu@2dfire.com>
2019-07-25 20:10:25 +0800

    新增题解

--- src/main/java/com/sugu/solution/base/ListNode.java
+++ src/main/java/com/sugu/solution/base/ListNode.java
@@ -1,10 +1,14 @@
 package com.sugu.solution.base;
 
+/**
+ * @author: create by sugu
+ * @date:2019/7/25
+ */
 public class ListNode {
     public int val;
     public ListNode next = null;
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 }
