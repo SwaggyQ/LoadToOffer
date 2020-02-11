4d21a5e33b588a02a745809b3abd7b705f8288f5
Gu <guquanye@didiglobal.com>
2020-02-11 16:48:51 +0800

    feat: 新增链表题解

--- src/main/java/com/sugu/solution/linkedlist/排序链表.java
+++ src/main/java/com/sugu/solution/linkedlist/排序链表.java
@@ -11,25 +11,9 @@ import com.sugu.solution.base.ListNode;
 public class 排序链表  {
 
     public ListNode sortList(ListNode head) {
-        if(head == null){
-            return null;
-        }
-        return helper(head,head.next);
-    }
+        // 头结点
+        ListNode pre = head;
 
-    public ListNode helper(ListNode l1, ListNode l2){
-        if(l1 == null){
-            return l2;
-        }
-        if(l2 == null){
-            return l1;
-        }
-        if(l1.val > l2.val){
-            ListNode next = helper(l1,l2.next);
-            return l2;
-        }else {
-            ListNode next = helper(l1.next,l2);
-            return l1;
-        }
+        return pre;
     }
 }
