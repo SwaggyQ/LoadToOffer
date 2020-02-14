c1541e592621ca860da1da270a65e5469e53418b
SwaggyQ <516535441@qq.com>
2020-02-14 16:06:52 +0800

    feat: add linkedlist solutions

---
+++ src/main/java/com/sugu/solution/linkedlist/at20200214/两数相加2.java
@@ -0,0 +1,44 @@
+package com.sugu.solution.linkedlist.at20200214;
+
+import com.sugu.solution.base.ListNode;
+
+/**
+ * @author: Gu Quanye
+ * @Date: 2020/2/14 2:02 下午
+ * @Number: 2
+ * @Link: https://leetcode-cn.com/problems/add-two-numbers/
+ */
+public class 两数相加2 {
+    public static void main(String[] args) {
+        ListNode node = new ListNode(8);
+        ListNode node1 = new ListNode(9);
+        ListNode node3 = new ListNode(9);
+
+        node.next = node1;
+        node1.next = node3;
+
+        ListNode node2 = new ListNode(2);
+        addTwoNumbers(node,node2);
+    }
+    // 1,8   0
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int cache = 0;
+        while ( l1 != null || l2 != null){
+            int l1V = l1 == null ? 0 : l1.val;
+            int l2V = l2 == null ? 0 : l2.val;
+            int val = l1V + l2V + cache;
+            int cacheV = val % 10;
+            cache = val / 10;
+            curr.next = new ListNode(cacheV);
+            curr = curr.next;
+            l1 = l1 == null? null : l1.next;
+            l2 = l2 == null? null : l2.next;
+        }
+        if(cache != 0){
+            curr.next = new ListNode(cache);
+        }
+        return dummy.next;
+    }
+}
---
+++ src/main/java/com/sugu/solution/linkedlist/删除链表的倒数第N个节点.java
@@ -0,0 +1,60 @@
+package com.sugu.solution.linkedlist;
+
+import com.sugu.solution.base.ListNode;
+
+/**
+ * @author: Gu Quanye
+ * @Date: 2020/2/14 9:58 上午
+ * @Number: 19
+ * @Link: https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+ */
+public class 删除链表的倒数第N个节点 {
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        removeNthFromEnd(node,1);
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null){
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode fast = head;
+        while (n-- > 0){
+            fast = fast.next;
+        }
+        while (fast != null){
+            fast = fast.next;
+            dummy = dummy.next;
+        }
+        ListNode curr = dummy.next.next;
+        dummy.next = curr;
+        return pre.next;
+    }
+
+
+    public static ListNode removeNthFromEnd1(ListNode head, int n) {
+        if (head == null){
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n != 0){
+            fast = fast.next;
+            n--;
+        }
+        if (fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+
+
+    }
+}
