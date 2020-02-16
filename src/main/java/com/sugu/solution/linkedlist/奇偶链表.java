3588968f5a8e05e066d5851a2e886df2f3bb28e7
SwaggyQ <516535441@qq.com>
2020-02-16 19:34:04 +0800

    feat: add linkedlist solutions

---
+++ src/main/java/com/sugu/solution/linkedlist/奇偶链表.java
@@ -0,0 +1,27 @@
+package com.sugu.solution.linkedlist;
+
+import com.sugu.solution.base.ListNode;
+
+/**
+ * @author: Gu Quanye
+ * @Date: 2020/2/14 2:30 下午
+ * @Number: 328
+ * @Link: https://leetcode-cn.com/problems/odd-even-linked-list/
+ */
+public class 奇偶链表 {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+
+    }
+}
