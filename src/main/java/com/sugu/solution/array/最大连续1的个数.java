e4074978b6c5a229417c79b43485e05a2544686b
SwaggyQ <516535441@qq.com>
2020-03-02 22:23:50 +0800

    feat: add array  solutions

---
+++ src/main/java/com/sugu/solution/array/最大连续1的个数.java
@@ -0,0 +1,32 @@
+package com.sugu.solution.array;
+
+/**
+ * @author: Gu Quanye
+ * @Date: 2020/3/2 1:29 下午
+ * @Number: 485
+ * @Link: https://leetcode-cn.com/problems/max-consecutive-ones/
+ */
+public class 最大连续1的个数 {
+
+    // [1,1,0,1,1,1]
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int len = nums.length;
+        int left = 0;
+        int right = 0;
+        int cnt = 0;
+        while (right < len){
+            if(nums[right] == 1){
+                right++;
+            }else {
+                cnt = Math.max(cnt , right-left);
+                left = right + 1;
+                right++;
+            }
+        }
+        cnt = Math.max(cnt , right-left);
+        return cnt;
+    }
+}
