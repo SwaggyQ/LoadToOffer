fb24ceafffe26a846604e0865c0baa99045d90b3
SwaggyQ <516535441@qq.com>
2020-08-20 23:08:11 +0800

    feat: 新增树遍历题解

---
+++ leetcode/editor/cn/[144]二叉树的前序遍历.java
@@ -0,0 +1,51 @@
+//给定一个二叉树，返回它的 前序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [1,2,3]
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 
+// 👍 349 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> res = new LinkedList<>();
+        if(root == null){
+            return res;
+        }
+        stack.add(root);
+        while( !stack.isEmpty()){
+            TreeNode node = stack.pollLast();
+            res.add(node.val);
+            if(node.right != null){
+                stack.add(node.right);
+            }
+            if(node.left != null){
+                stack.add(node.left);
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[145]二叉树的后序遍历.java
@@ -0,0 +1,45 @@
+//给定一个二叉树，返回它的 后序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [3,2,1] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 
+// 👍 376 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> list = new LinkedList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        helper(root);
+        return list;
+    }
+    public void helper(TreeNode root){
+        if(root == null){
+            return;
+        }
+        helper(root.left);
+        helper(root.right);
+        list.add(root.val);
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
